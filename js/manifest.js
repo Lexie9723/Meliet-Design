b96a63d1b0f1d623656edced9c0fa4f8df78ee10
Lexie9723 <1351581045@qq.com>
2021-06-26 15:25:03 +0800

    init

---
+++ js/manifest.js
@@ -0,0 +1,15 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '5d6b6a4ecec589741d5cfc7b';
+_mrmcp['creative_id'] = '5ede34942ce5c1548d789d4e';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 520;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '小米设计生态研究';
+_mrmcp['build_number'] = parseInt('355');
+_mrmcp['publish_time'] = 'Tue Jun 09 2020 15:31:40 GMT+0800 (CST)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+})();
